faa8c10c2f6bb5c1869ebac760cbb98d8dc7f9b5
Bogeun, Kim <127417159+Kim-Bogeun@users.noreply.github.com>
2025-04-04 11:55:27 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,13 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    int m;
+   
+    cin >> n >> m;
+    
+    int result = (n-1)+n*(m-1);
+    
+    cout << result << endl;
+}
