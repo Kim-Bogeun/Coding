7fbd4114972562f005c55c89c85e31f351d9b86e
Bogeun, Kim <127417159+Kim-Bogeun@users.noreply.github.com>
2025-08-30 15:49:07 +0900

    Easy / Time: 10 ms (40.04%), Space: 17.7 MB (64.45%)

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        rev, tmp = 0, x
+        while tmp > 0:
+            rev = rev * 10 + tmp % 10
+            tmp //= 10
+
+        return x == rev
